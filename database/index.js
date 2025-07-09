b6720639a66435ca4c8045bec9474681f75308df
Joseph Collado <sourpina@gmail.com>
2025-07-09 19:11:25 -0400

    Added association aliases

--- database/index.js
+++ database/index.js
@@ -4,14 +4,14 @@ const User = require("./user");
 
 // ONE TO MANY
 // A task has ONE user-owner
-User.hasMany(Task);
 Task.belongsTo(User);
+User.hasMany(Task, { as: "ownedTasks" });
 
 // MANY TO MANY
 // A task may have many users assigned to them
 // Users may be assigned to many tasks
-User.belongsToMany(Task, { through: "assignments" });
-Task.belongsToMany(User, { through: "assignments" });
+User.belongsToMany(Task, { through: "assignments", as: "assignedTasks" });
+Task.belongsToMany(User, { through: "assignments", as: "assignees" });
 
 // Export everything needed
 module.exports = {
